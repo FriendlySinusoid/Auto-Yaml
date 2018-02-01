07f675c344a5969c7b07d0bd2c85e66946a3a17b
unknown <s.f.skarpiphi@gmail.com>
2018-02-01 16:34:04 -0500

    Fixed Some Stuff

--- src/main/java/PathGenerator.java
+++ src/main/java/PathGenerator.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -13,181 +16,152 @@ import java.util.Scanner;
 
 public class PathGenerator extends JFrame implements KeyListener, MouseListener {
 
-	private BufferedImage background = ImageIO.read(new File("Field.png"));
-	private BufferedImage bufferImage = new BufferedImage(1080, 540, BufferedImage.TYPE_INT_RGB);
-	private ArrayList<double[]> path = new ArrayList<>();
-	private Graphics buffer = bufferImage.getGraphics();
-	private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private BufferedImage background = ImageIO.read(new File("Field.png"));
+    private BufferedImage bufferImage = new BufferedImage(1080, 540, BufferedImage.TYPE_INT_RGB);
+    private ArrayList<double[]> path = new ArrayList<>();
+    private Graphics buffer = bufferImage.getGraphics();
+    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
-	private enum Mode {
-		path,
-		heading
-	}
+    private enum Mode {
+        path,
+        heading
+    }
 
-	private int headingIndex = 0;
+    private int headingIndex = 0;
 
-	private Mode mode = Mode.path;
+    private Mode mode = Mode.path;
 
-	public static void main(String args[]) {
-		try {
-			PathGenerator pg = new PathGenerator();
-			pg.paint(pg.getGraphics());
-			while (true) {
-				pg.repaint();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+    public static void main(String args[]) {
+        try {
+            PathGenerator pg = new PathGenerator();
+            pg.paint(pg.getGraphics());
+            while (true) {
+                pg.repaint();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-	}
+    }
 
-	public PathGenerator() throws IOException {
-		this.setSize(1080, 540);
-		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.addMouseListener(this);
-		this.addKeyListener(this);
-		this.setVisible(true);
-	}
+    public PathGenerator() throws IOException {
+        this.setSize(1080, 540);
+        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.addMouseListener(this);
+        this.addKeyListener(this);
+        this.setVisible(true);
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		buffer.drawImage(background, 0, 0, 1080, 540, null);
-		if (path.size() >= 2) {
-			buffer.setColor(Color.RED);
-			double[] last = path.get(0);
-			for (int i = 1; i < path.size(); i++) {
-				buffer.drawLine((int) last[0], (int) last[1], (int) path.get(i)[0], (int) path.get(i)[1]);
-				last = path.get(i);
-			}
-		}
+    @Override
+    public void paint(Graphics g) {
+        buffer.drawImage(background, 0, 0, 1080, 540, null);
+        if (path.size() >= 2) {
+            buffer.setColor(Color.RED);
+            double[] last = path.get(0);
+            for (int i = 1; i < path.size(); i++) {
+                buffer.drawLine((int) last[0], (int) last[1], (int) path.get(i)[0], (int) path.get(i)[1]);
+                last = path.get(i);
+            }
+        }
 
-		if (mode == Mode.heading) {
-			buffer.setColor(Color.blue);
-			buffer.fillOval((int) path.get(headingIndex)[0] - 4, (int) path.get(headingIndex)[1] - 4, 8, 8);
-			buffer.drawLine((int) path.get(headingIndex)[0],
-					(int) path.get(headingIndex)[1], (int) path.get(headingIndex)[0] + (int) (30 * Math.cos(path.get(headingIndex)[2])), (int) path.get(headingIndex)[1] + (int) (30 * Math.sin(path.get(headingIndex)[2])));
+        if (mode == Mode.heading) {
+            buffer.setColor(Color.blue);
+            buffer.fillOval((int) path.get(headingIndex)[0] - 4, (int) path.get(headingIndex)[1] - 4, 8, 8);
+            buffer.drawLine((int) path.get(headingIndex)[0],
+                    (int) path.get(headingIndex)[1], (int) path.get(headingIndex)[0] + (int) (30 * Math.cos(path.get(headingIndex)[2])), (int) path.get(headingIndex)[1] + (int) (30 * Math.sin(path.get(headingIndex)[2])));
 
-		}
+        }
 
-		g.drawImage(bufferImage, 0, 0, null);
+        g.drawImage(bufferImage, 0, 0, null);
 
-	}
+    }
 
-	@Override
-	public void keyTyped(KeyEvent e) {
+    @Override
+    public void keyTyped(KeyEvent e) {
 
-	}
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_U && mode == Mode.path) {
-			path.remove(path.size() - 1);
-		}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_U && mode == Mode.path) {
+            path.remove(path.size() - 1);
+        }
 
-		if (e.getKeyCode() == KeyEvent.VK_S && mode == Mode.path) {
-			int answer = JOptionPane.showConfirmDialog(this, "Move onto headings?");
-			if (answer == 0)
-				mode = Mode.heading;
-		}
+        if (e.getKeyCode() == KeyEvent.VK_S && mode == Mode.path) {
+            int answer = JOptionPane.showConfirmDialog(this, "Move onto headings?");
+            if (answer == 0)
+                mode = Mode.heading;
+        }
 
 
-		if (e.getKeyCode() == KeyEvent.VK_S && mode == Mode.heading) {
-			int answer = JOptionPane.showConfirmDialog(this, "Save Final Map?");
-			if (answer == 0) {
-				try {
-					saveMaptoYAML();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-					JOptionPane.showMessageDialog(this, "Error | IOException in Saving Map");
-				}
-			}
-		}
+        if (e.getKeyCode() == KeyEvent.VK_S && mode == Mode.heading) {
+            int answer = JOptionPane.showConfirmDialog(this, "Save Final Map?");
+            if (answer == 0) {
+                try {
+                    saveMaptoYAML();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    JOptionPane.showMessageDialog(this, "Error | IOException in Saving Map");
+                }
+            }
+        }
 
-		if (e.getKeyCode() == KeyEvent.VK_ENTER && mode == Mode.heading && headingIndex < path.size() - 1) {
-			headingIndex++;
-		}
+        if (e.getKeyCode() == KeyEvent.VK_ENTER && mode == Mode.heading && headingIndex < path.size() - 1) {
+            headingIndex++;
+        }
 
-		if (e.getKeyCode() == KeyEvent.VK_SHIFT && mode == Mode.heading && headingIndex > 0) {
-			headingIndex--;
-		}
+        if (e.getKeyCode() == KeyEvent.VK_SHIFT && mode == Mode.heading && headingIndex > 0) {
+            headingIndex--;
+        }
 
-	}
+    }
 
-	private void saveMaptoYAML() throws IOException {
-		boolean isFileCreated = false;
-		Scanner fileScanner = new Scanner(new File("BaseFileHeading.txt"));
+    private void saveMaptoYAML() throws IOException {
+        Scanner fileScanner = new Scanner(new File("BaseFileHeading.txt"));
 
-		while (!isFileCreated){
 
-		final class YAML implements java.io.Serializable{
-			private String userDirectory = new String();
-			private String fileName = new String();
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.writeValue(new File(System.getProperty("user.dir")), new Scanner(System.in));
+    }
 
-			public YAML(String userDirectory, String fileName){
-				this.userDirectory = userDirectory;
-				this.fileName = fileName;
-			}
+    @Override
+    public void keyReleased(KeyEvent e) {
 
-			public String getUserDirectory() {
-				return userDirectory;
-			}
+    }
 
-			public void setUserDirectory(String userDirectory) {
-				this.userDirectory = userDirectory;
-			}
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (mode == Mode.path)
+            path.add(new double[]{e.getX(), e.getY(), 0});
+        if (mode == Mode.heading) {
+            double angle = 0;
+            angle = Math.atan((path.get(headingIndex)[1] - e.getY()) / (path.get(headingIndex)[0] - e.getX()));
+            if (path.get(headingIndex)[0] - e.getX() > 0)
+                angle -= Math.PI;
+            path.get(headingIndex)[2] = angle;
+        }
+    }
 
-			public String getFileName() {
-				return fileName;
-			}
+    @Override
+    public void mousePressed(MouseEvent e) {
 
-			public void setFileName(String fileName) {
-				this.fileName = fileName;
-			}
-		}
+    }
 
-			YAML PathYAML = new YAML(System.getProperty("user.dir"), JOptionPane.showInputDialog("Enter File Name") + ".yml");
+    @Override
+    public void mouseReleased(MouseEvent e) {
 
-		}
-	}
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
+    @Override
+    public void mouseEntered(MouseEvent e) {
 
-	}
+    }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if (mode == Mode.path)
-			path.add(new double[]{e.getX(), e.getY(), 0});
-		if (mode == Mode.heading) {
-			double angle = 0;
-			angle = Math.atan((path.get(headingIndex)[1] - e.getY()) / (path.get(headingIndex)[0] - e.getX()));
-			if (path.get(headingIndex)[0] - e.getX() > 0)
-				angle -= Math.PI;
-			path.get(headingIndex)[2] = angle;
-		}
-	}
+    @Override
+    public void mouseExited(MouseEvent e) {
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-
-	}
+    }
 }
